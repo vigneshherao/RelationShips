0d67221f5988e96e3103a7a7f87f33c23b53005e
vignesh <vigneshfornavy@gmail.com>
2023-12-06 09:03:57 +0530

    one to few

--- index.js
+++ index.js
@@ -11,36 +11,33 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/realtion');  
 }
 
-
-const userSchema =new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username:String,
-    address:[{
-        _id:false,
-        location:String,
-        city:String
-    }]
+    address:[
+        {
+            _id:false,
+            location:String,city:String,
+        }
+    ]
 });
 
 const User = mongoose.model("User",userSchema);
 
-
-
 const addUser = async()=>{
     let user1 = new User({
-        username:"Vignesh",
-        address:[
-            {
-                location:"manglore",
-                city:"manglore",
-            }
-        ]
+        username:"vignesh",
+        address:[{
+            location:"kanhangad",
+            city:"manglore"
+        }],
     })
 
+    user1.address.push({location:"Banglore",city:"Manglore"});
 
-    user1.address.push({location:"Puttur",city:"Banglore"});
     let result = await user1.save();
     console.log(result);
-}
 
+
+}
 
 addUser();
